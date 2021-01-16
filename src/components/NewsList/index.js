f2e387b0cedfc4e8f36bd68ed1af73ad76ec7870
Amirreza Esm <amirreza.77.esmaeili@gmail.com>
2021-01-17 02:17:51 +0330

    add no results check

--- src/components/NewsList/index.js
+++ src/components/NewsList/index.js
@@ -1,13 +1,16 @@
+import { Icon } from "semantic-ui-react";
 import NewsItem from "../NewsItem";
 
 import "./style.css";
 
 export default function NewsList({ allNews }) {
   return (
+    allNews?.length > 0 ?
     <div className="news-list">
       {allNews.map((news) => {
         return <NewsItem key={news.id} news={news} />;
       })}
     </div>
+    : <div className="no-result-message">No results found ... <Icon name="search"/></div>
   );
 }
